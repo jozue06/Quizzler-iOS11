fc7e6e434f3086a0f24bf940e03092228ce4d515
Jozue06 <jozue06@gmail.com>
2018-11-24 20:47:16 -0800

    f

---
+++ Quizzler/Question.swift
@@ -0,0 +1,9 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Joshua McClung on 11/24/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Quizzler/QuestionBank.swift
@@ -0,0 +1,9 @@
+//
+//  QuestionBank.swift
+//  Quizzler
+//
+//  Created by Joshua McClung on 11/24/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
